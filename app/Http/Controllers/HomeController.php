218b073569dded92c72d4b41f6ba752f8926390b
Alberto Oficina <amengelle@preconcretoveracruz.com>
2020-08-26 14:58:27 -0500

    actualizacion de login

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Auth;
 class HomeController extends Controller
 {
     /**
@@ -14,6 +14,7 @@ class HomeController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+        //Auth::loginUsingId(1);
     }
 
     /**
@@ -22,7 +23,7 @@ class HomeController extends Controller
      * @return \Illuminate\Contracts\Support\Renderable
      */
     public function index()
-    {
+    {        
         return view('home');
     }
 }
